b07275b975221a7fb4c29fc78653d6494f986a38
Eduardo-Bernal <cbedudu@gmail.com>
2026-03-05 08:05:17 -0300

    merging error

--- Royal_Games/DTOs/AutenticacaoDto/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Royal_Games.DTOs.AutenticacaoDto
-{
-    public class LoginDto
-    {
-        public string Email { get; set; } = null!;
-        public string Senha { get; set; } = null!;
-    }
-}
--- Royal_Games/Interfaces/IGeneroRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Royal_Games.Domains;
-
-namespace Royal_Games.Interfaces
-{
-    public class IGeneroRepository
-    {
-    }
-}
